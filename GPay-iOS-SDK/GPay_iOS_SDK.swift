c491b0f9c420469760bc7654e1703ac53cdda8cb
Basem Elazzabi <basim@libyaguide.net>
2025-06-11 14:23:38 +0200

    Initial Commit

---
+++ GPay-iOS-SDK/GPay_iOS_SDK.swift
@@ -0,0 +1,9 @@
+//
+//  GPay_iOS_SDK.swift
+//  GPay-iOS-SDK
+//
+//  Created by Basem Elazzabi on 11/6/2025.
+//
+
+import Foundation
+
---
+++ GPay-iOS-SDKTests/GPay_iOS_SDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  GPay_iOS_SDKTests.swift
+//  GPay-iOS-SDKTests
+//
+//  Created by Basem Elazzabi on 11/6/2025.
+//
+
+import Testing
+@testable import GPay_iOS_SDK
+
+struct GPay_iOS_SDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
